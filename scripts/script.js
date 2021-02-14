83836f2c2227c70c0de792f22ebb1c5b247958f3
Michael Maddy <MichaelMvv@users.noreply.github.com>
2021-02-13 21:32:46 -0800

    Part1 and 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
 // Script.js
-
+let productArray;
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  productArray = localStorage.getItem('ProductArray');
+  if (fromLocalStorage === null) {
+    fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(data => {
+          localStorage.setItem('ProductArray', toString(data));
+          productArray = data;
+        });
+  }
 });
