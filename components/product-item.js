141c3f339e07cf02f4efd5164e7f45d708b74294
Michael Maddy <MichaelMvv@users.noreply.github.com>
2021-02-13 22:14:42 -0800

    Items pop up now

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,107 @@
 // product-item.js
 
+// <!-- li class="product">
+//     <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+//     alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
+//     <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15
+//     Laptops</p> <p class="price">$109.95</p> <button onclick="alert('Added to
+//     Cart!')">Add to Cart</button>
+//           </li -->
+
+
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor(foo) {
+    super();
+    this.attachShadow({mode: 'open'});
+
+    const wrapper = document.createElement('li');
+    wrapper.setAttribute('class', 'product');
+
+    const img = wrapper.appendChild(document.createElement('img'));
+    img.setAttribute('src', foo['image']);
+    img.setAttribute('alt', foo['title']);
+
+    const title = wrapper.appendChild(document.createElement('p'));
+    title.setAttribute('class', 'title');
+    title.textContent = foo['title'];
+
+    const price = wrapper.appendChild(document.createElement('p'));
+    price.setAttribute('class', 'price');
+    price.textContent = '$' + foo['price'];
+
+    const button = wrapper.appendChild(document.createElement('button'));
+    button.textContent = 'Add to Cart';
+
+
+    // I don't like this....
+    const style = document.createElement('style');
+    style.textContent = `
+    .price {
+      color: green;
+      font-size: 1.8em;
+      font-weight: bold;
+      margin: 0;
+    }
+    
+    .product {
+      align-items: center;
+      background-color: white;
+      border-radius: 5px;
+      display: grid;
+      grid-template-areas: 
+      'image'
+      'title'
+      'price'
+      'add';
+      grid-template-rows: 67% 11% 11% 11%;
+      height: 450px;
+      filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+      margin: 0 30px 30px 0;
+      padding: 10px 20px;
+      width: 200px;
+    }
+    
+    .product > button {
+      background-color: rgb(255, 208, 0);
+      border: none;
+      border-radius: 5px;
+      color: black;
+      justify-self: center;
+      max-height: 35px;
+      padding: 8px 20px;
+      transition: 0.1s ease all;
+    }
+    
+    .product > button:hover {
+      background-color: rgb(255, 166, 0);
+      cursor: pointer;
+      transition: 0.1s ease all;
+    }
+    
+    .product > img {
+      align-self: center;
+      justify-self: center;
+      width: 100%;
+    }
+    
+    .title {
+      font-size: 1.1em;
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    }
+    `;
+    this.shadowRoot.append(style, wrapper);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,19 @@
 // Script.js
-let productArray;
 window.addEventListener('DOMContentLoaded', () => {
-  productArray = localStorage.getItem('ProductArray');
-  if (fromLocalStorage === null) {
+  if (localStorage.getItem('ProductArray') === null) {
     fetch('https://fakestoreapi.com/products')
         .then(response => response.json())
         .then(data => {
-          localStorage.setItem('ProductArray', toString(data));
-          productArray = data;
-        });
+          localStorage.setItem('ProductArray', JSON.stringify(data));
+        })
+        .then(addProducts());
+  } else {
+    addProducts();
   }
 });
+
+function addProducts() {
+  let productHolder = document.getElementById('product-list');
+  let bar = JSON.parse(localStorage.getItem('ProductArray'));
+  bar.forEach(element => productHolder.appendChild(new ProductItem(element)));
+}
